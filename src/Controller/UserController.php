2f82d9d89bc35df5568d1d5fa3f4cab5eae821bf
Obhino <64480482+Obhino@users.noreply.github.com>
2022-09-24 15:09:13 +0100

    ajout d'une page utilisateur et formattage de la route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\BoutiqueFormType;
+use App\Repository\BoutiqueRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,4 +24,56 @@ class UserController extends AbstractController
             'user_name'=>$user->getUserIdentifier(),
         ]);
     }
+    #[Route(path:'/{username}/admin', name:'app_shop_admin')]
+    public function shopadmin(Request $request, UserRepository $userRepository, BoutiqueRepository $boutiqueRepository): Response
+    {
+        $user = $this->getUser();
+        $userClass = $userRepository->findOneBy(['username'=>$user->getUserIdentifier()]);
+        $listboutiques = $boutiqueRepository->findBy(['proprietaire'=>$userClass]);
+
+        dump($userClass);
+        dump($listboutiques);
+        return $this->render('user/admin.html.twig',[
+            'username'=>$user->getUserIdentifier(),
+            'boutiques'=>$listboutiques,
+        ]);
+
+    }
+    #[Route(path:'/{username}/admin/{shop}', name:'app_admin_ashop')]
+    public function adminAshop(Request $request, $username, $shop, EntityManagerInterface $entityManager, BoutiqueRepository $boutiqueRepository, UserRepository $userRepository): Response{
+        $boutique = $boutiqueRepository->findOneBy(['nom'=>$shop]);
+        $user = $boutique->getProprietaire();
+        if(!$username==$user->getUsername())
+        {
+            $this->redirectToRoute('app_main');
+        }
+        $form = $this->createForm(BoutiqueFormType::class, $boutique);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $logoFile = $form->get('logo')->getData();
+            if($logoFile)
+            {
+                $original_file_name = pathinfo($logoFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $filename= bin2hex(random_bytes(6).'.'.$logoFile->guessExtension());
+
+            }
+            try {
+                $logoFile->move(
+                    $this->getParameter('logo_dir'),
+                    $filename);
+
+            }catch (FileException $exception){
+                $boutique->setLogo($filename);
+            }
+            $entityManager->persist($boutique);
+            $entityManager->flush();
+        }
+        return $this->renderForm('user/adminAshop.html.twig',[
+            'admin_shop_form'=>$form,
+            'boutique'=>$boutique,
+            'user'=>$user,
+        ]);
+    }
+
 }
